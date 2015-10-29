d6e88c3d1c7d544c05c1c93b2f5a956f1f7f0945
Aleh Pahireichyk <ialeh@yandex.ru>
2015-10-29 19:47:44 +0300

    02

---
+++ MoreLessProgram/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreLessProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string pathInPut = Path.Combine(path, "INPUT.txt");
+
+            //using (StreamWriter writer = File.CreateText(pathInPut))
+            //{
+            //    writer.WriteLine(7);
+            //    writer.WriteLine(5);
+            //}
+
+            string aStr = String.Empty;
+            string bStr = String.Empty;
+
+            using (StreamReader reader = File.OpenText(pathInPut))
+            {
+                aStr = reader.ReadLine();
+                bStr = reader.ReadLine();
+
+            }
+
+            int a = int.Parse(aStr);
+            int b = int.Parse(bStr);
+
+            string pathOutPut = Path.Combine(path, "OUTPUT.txt");
+
+            using (StreamWriter writer = File.CreateText(pathOutPut))
+            {
+                if (a < b)
+                {
+                    writer.WriteLine("<");
+                }
+                else if (a > b)
+                {
+                    writer.WriteLine(">");
+                }
+                else writer.WriteLine("=");
+            }
+
+        }
+    }
+}
